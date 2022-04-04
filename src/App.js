3fa0961cf815dd58473a2856cec5a06a0f2c5491
SazidAhmed <sazidahmed.official@gmail.com>
2022-04-04 13:52:08 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Dashboard from './pages/admin/Dashboard'
+import Login from './pages/guest/Login'
+import Register from './pages/guest/Register'
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path='/' element={< Dashboard />} />
+            <Route path='/login' element={< Login />} />
+            <Route path='/register' element={< Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
+
 
 export default App;
