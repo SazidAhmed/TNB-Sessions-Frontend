7ae0a8aea8b2d10f6960f034dbc9d405aee8d074
SazidAhmed <sazidahmed.official@gmail.com>
2022-04-05 18:17:24 +0600

    register login logout completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 import Header from './components/Header';
 import Dashboard from './pages/admin/Dashboard'
 import Login from './pages/guest/Login'
 import Register from './pages/guest/Register'
+
 
 function App() {
   return (
